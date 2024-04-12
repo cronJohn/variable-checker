2dda5e6ae6ce356e280563f6352246d24fa86f4c
cronJohn <51012201+cronJohn@users.noreply.github.com>
2024-04-11 20:53:12 -0500

    Update UI to better show variable use conditions

--- src/components/language/index.tsx
+++ src/components/language/index.tsx
@@ -1,4 +1,7 @@
+import { Tooltip } from "@kobalte/core";
 import { Icon } from '@iconify-icon/solid';
+import { Show } from "solid-js";
+import './style.css';
 
 export function Language(props: any){
     // skill-icons
@@ -15,10 +18,25 @@ export function Language(props: any){
         "Ruby": "ruby",
         "Perl": "perl",
     }
+    const lang: string = props.language.split(' ')[0];
+    const desc: string = props.language.split(' ')[1];
+
     return (
         <div class="flex items-center w-50% p-3 rounded-lg shadow-sm text-light bg-dark-light">
-            <span class="mr-auto font-bold">{props.language}</span>
-            <Icon icon={`skill-icons:${langToIconMapping[props.language.split(' ')[0]]}`} width="32" height="32"/>
+            <span class="mr-auto">{lang}</span>
+            <Show when={desc}>
+                <Tooltip.Root openDelay={100} closeDelay={100}>
+                      <Tooltip.Trigger class="tooltip__trigger h-2rem font-code bg-second text-light rd-8px">{desc} only</Tooltip.Trigger>
+                      <Tooltip.Portal>
+                        <Tooltip.Content class="tooltip__content b-1px b-solid b-light bg-dark px-1rem rd-8px">
+                          <Tooltip.Arrow class="text-dark"/>
+                          <p class="font-code text-light">Only allowed for {desc} variables</p>
+                        </Tooltip.Content>
+                      </Tooltip.Portal>
+                </Tooltip.Root>
+                <span class="ml-2"></span>
+            </Show>
+            <Icon icon={`skill-icons:${langToIconMapping[lang]}`} width="32" height="32"/>
         </div>
     );
 }
