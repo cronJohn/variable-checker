44aa9fb1f2f74f1550592fbce115d2a785f153ce
cronJohn <51012201+cronJohn@users.noreply.github.com>
2024-04-11 21:28:56 -0500

    Fix list spacing issue
    Now has use condition and icon on the right

--- src/components/language/index.tsx
+++ src/components/language/index.tsx
@@ -22,11 +22,11 @@ export function Language(props: any){
     const desc: string = props.language.split(' ')[1];
 
     return (
-        <div class="flex items-center w-50% p-3 rounded-lg shadow-sm text-light bg-dark-light">
-            <span class="mr-auto">{lang}</span>
+        <div class="flex items-center justify-between w-50% p-3 rounded-lg shadow-sm text-light bg-dark-light">
+            <span class="">{lang}</span>
             <Show when={desc}>
                 <Tooltip.Root openDelay={100} closeDelay={100}>
-                      <Tooltip.Trigger class="tooltip__trigger h-2rem font-code bg-second text-light rd-8px">{desc} only</Tooltip.Trigger>
+                      <Tooltip.Trigger class="tooltip__trigger ml-auto mr-1 h-2rem font-code bg-second text-light rd-8px">{desc} only</Tooltip.Trigger>
                       <Tooltip.Portal>
                         <Tooltip.Content class="tooltip__content b-1px b-solid b-light bg-dark px-1rem rd-8px">
                           <Tooltip.Arrow class="text-dark"/>
@@ -34,7 +34,6 @@ export function Language(props: any){
                         </Tooltip.Content>
                       </Tooltip.Portal>
                 </Tooltip.Root>
-                <span class="ml-2"></span>
             </Show>
             <Icon icon={`skill-icons:${langToIconMapping[lang]}`} width="32" height="32"/>
         </div>
